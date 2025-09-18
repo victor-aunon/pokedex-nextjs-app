5576c3c4277229454e70b2c8acc57f247291b143
victor-aunon <hola@victoraunon.com>
2025-09-18 03:45:25 +0200

    fix: next config complaining about turbopack root

--- next.config.js
+++ next.config.js
@@ -3,8 +3,17 @@
  * for Docker builds.
  */
 import './src/env.js'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 /** @type {import("next").NextConfig} */
-const config = {}
+const config = {
+	turbopack: {
+		root: __dirname,
+	},
+}
 
 export default config
